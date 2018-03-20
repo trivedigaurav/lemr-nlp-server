d823792994b3fe2ae3903401c7c15f9430fc53d0
Gaurav Trivedi <trivedigaurav@gmail.com>
2018-03-20 13:39:04 -0400

    Add todo comment

--- server/login.py
+++ server/login.py
@@ -1,9 +1,10 @@
 import json
 import falcon
 
-
 class Login(object):
 
+    #There is no logic to check login here
+    #Relying on server configurations for this
     def on_get(self, req, resp):
         resp.body = '{"message": "OK"}'
         resp.status = falcon.HTTP_200
