3b3cb6d8d8483b9f201c8dbe24f9fec8559404ab
Gaurav Trivedi <trivedigaurav@gmail.com>
2018-03-20 13:38:16 -0400

    Remove falcon-auth

--- server/app.py
+++ server/app.py
@@ -1,13 +1,12 @@
 import falcon
 from falcon_cors import CORS
-from falcon_auth import FalconAuthMiddleware, BasicAuthBackend
 
 from .test import Resource
 from .login import Login
 from .getreport import *
-from .logger import *
 from .annotator import *
 from .getencounter import *
+from .putlogevent import *
 
 from .database import *
 database = connection['lemr']
@@ -18,11 +17,7 @@ cors = CORS(allow_origins_list=['http://localhost:8000'],
             allow_all_headers=True,
             allow_all_methods=True)
 
-#falcon-auth
-user_loader = lambda username, password: { 'username': username }
-auth_middleware = FalconAuthMiddleware(BasicAuthBackend(user_loader))
-
-application = falcon.API(middleware=[cors.middleware, auth_middleware])
+application = falcon.API(middleware=[cors.middleware])
 
 test = Resource()
 application.add_route('/test', test)
