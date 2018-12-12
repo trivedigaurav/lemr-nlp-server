207faec7f763983d5c527c99cfd05ee2762d87d4
Gaurav Trivedi <trivedigaurav@gmail.com>
2018-12-12 01:19:39 -0500

    Get all predictions
    Note mongodb should have indices built for fast query...

--- server/getpredictions.py
+++ server/getpredictions.py
@@ -4,6 +4,9 @@ from .logger import logEvent
 from ast import literal_eval
 from collections import defaultdict
 
+##TODO: Use actual predictions and not gold standards 
+class_var = "gold_label"
+
 class GetPredictions(object):
 
     def __init__(self, database):
@@ -11,36 +14,75 @@ class GetPredictions(object):
 
     def on_get(self, req, resp, encounterid, modelid="current"):
         
-        #TODO: Handle models
-        ##TODO: Use actual predictions and not gold standards 
+        def _run_query(level, encounterid):
+            return self.db[level].find( { "encounter_id": str(encounterid) }, {"text": 0} ) \
+                            .sort([("date",1)])
 
+        def _clean_row(row):
+            row["_id"] = str(row["_id"])
+            row['rationales'] = literal_eval(row['rationales'])
+            row['class'] = row[class_var]
+
+            return row
+
+        #TODO: Handle model revisions
 
         if encounterid != None:
 
-            row = self.db.encounters.find_one( { "encounter_id": str(encounterid) })
+            enc = self.db.encounters.find_one( { "encounter_id": str(encounterid) })
 
-            if (row):
+            if (enc):
 
                 message = {
-                    "class": row['gold_label'],
-                    "rationales": literal_eval(row['rationales'])
+                    "class": enc[class_var],
+                    "rationales": literal_eval(enc['rationales']),
+                
+                    "reports": defaultdict(list),
+                    "sections": defaultdict(list),
+                    "sentences": defaultdict(list),
+
+                    "pos_reports": [],
+                    "pos_sections": [],
+                    "pos_sentences": []
                 }
 
 
-                levels = ["reports", "sections", "sentences"]
+                #Reports
+                for row in _run_query("reports", encounterid):
+                    row_ = _clean_row(row)
+                    id_ = row_["report_id"]
 
-                for level in levels:
-                    l_list = defaultdict(list)
+                    message["reports"][id_] = row_
+                    message["reports"][id_]["pos_sections"] = []
+                    message["reports"][id_]["pos_sentences"] = []
 
-                    for row in self.db[level].find( { "encounter_id": str(encounterid) }, {"text": 0} ).sort([("date",1)]):
-                        row["_id"] = str(row["_id"])
-                        row['rationales'] = literal_eval(row['rationales'])
+                    if(row_["class"] == "pos"):
+                        message["pos_reports"].append(id_)
 
-                        report_id = row.pop("report_id")
-                        
-                        l_list[report_id].append(row)
-                        
-                    message[level] = l_list
+                #Sections
+                for row in _run_query("sections", encounterid):
+                    row_ = _clean_row(row)
+                    id_ = row_["section_id"]
+
+                    message["sections"][id_] = row_
+                    message["sections"][id_]["pos_sentences"] = []
+
+                    if(row_["class"] == "pos"):
+                        message["pos_sections"].append(id_)
+                        message["reports"][row_["report_id"]]["pos_sections"].append(id_)
+
+
+                #Sentences
+                for row in _run_query("sentences", encounterid):
+                    row_ = _clean_row(row)
+                    id_ = row_["sentence_id"]
+
+                    message["sentences"][id_] = row_
+
+                    if(row_["class"] == "pos"):
+                        message["pos_sentences"].append(id_)
+                        message["reports"][row_["report_id"]]["pos_sentences"].append(id_)
+                        message["sections"][row_["section_id"]]["pos_sentences"].append(id_)
 
 
                 logEvent("getPredictionsByEncounter", str(message))
