ca8e15033c865b44d53241add43804b32275f8f0
Gaurav Trivedi <trivedigaurav@gmail.com>
2019-02-02 16:42:33 -0500

    Log to a specific log file only; Recreate if moved or deleted

--- server/logger.py
+++ server/logger.py
@@ -19,26 +19,33 @@ def set_up_logging():
     file_path = sys.modules[__name__].__file__
     project_path = os.path.dirname(os.path.dirname(file_path))
     log_location = project_path + '/logs/'
+    
     if not os.path.exists(log_location):
         os.makedirs(log_location)
 
-    current_time = datetime.now()
-    current_date = current_time.strftime("%Y-%m-%d")
-    file_name = current_date + '.log'
+    file_name = "run.log"
     file_location = log_location + file_name
     with open(file_location, 'a+'):
         pass
 
     logger = logging.getLogger(__name__)
     format = '[%(asctime)s] [%(levelname)s] [%(message)s]'
+
     # To store in file
     logging.basicConfig(format=format, filemode='a+', filename=file_location, level=logging.DEBUG)
+    
+    # Recreate file if deleted
+    log_handler = logging.handlers.WatchedFileHandler(file_location)
+    log_handler.setFormatter(logging.Formatter(format))
+    logger.addHandler(log_handler)
+
     # To print only
     # logging.basicConfig(format=format, level=logging.DEBUG)
-    return logger
+
+set_up_logging()
 
 def logEvent(event, message, level=20):
-    logger = set_up_logging()
+    logger = logging.getLogger(__name__)
     logger.log(level, event + ":\t"+ message)
 
 
