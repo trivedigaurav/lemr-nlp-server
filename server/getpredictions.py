f2f72a825a6fd139e7cea2b3782772c66731e67e
Gaurav Trivedi <trivedigaurav@gmail.com>
2019-02-13 23:03:36 -0500

    Use rationales for everything

--- server/getpredictions.py
+++ server/getpredictions.py
@@ -401,11 +401,10 @@ class GetPredictionsBase(object):
                 texts_.append(row['text'])
                 classes_.append(row['class'])
 
-                #add rationales for sentences
-                if (level == "sentences" and "rationale_list" in row):
-                    for rationale in row['rationale_list']:
-                        texts_.append(rationale)
-                        classes_.append(row['class'])
+                #add rationales
+                for rationale in row['rationale_list']:
+                    texts_.append(rationale)
+                    classes_.append(row['class'])
 
                 
             count_vect = CountVectorizer()
